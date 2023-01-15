46efd3edfe702af3fd9e710296315eb633dfa416
Ezequias Kluyvert de Oliveira Lemos <61207416+UserZeca@users.noreply.github.com>
2023-01-15 04:12:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button"
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Button text="Button"/>
+      <Button />
+    </div>
+  )
+}
+
+export default App
